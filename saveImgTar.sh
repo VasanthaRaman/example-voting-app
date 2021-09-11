cd05fd75013b8cc1aac6034fd10850cd17528da7
Vasantha Raman.A <43679625+VasanthaRaman@users.noreply.github.com>
2021-09-11 21:08:20 +0530

    update script

--- saveImgTar.sh
+++ saveImgTar.sh
@@ -2,6 +2,6 @@
 mkdir tars
 cp loadScript.sh tars/loadScript.sh
 docker save -o tars/vote-app.tar vasanth12/vote-app
-docker save -o tars/result-app.tar vasanth12/result-app
+docker save -o tars/node-app.tar vasanth12/node-app
 docker save -o tars/worker-app.tar vasanth12/worker-app
 
