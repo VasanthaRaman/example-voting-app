419ee3f9fb6f8b642ad8235d96679ea4ae1c36a8
VasanthaRaman <vasantharaman003@gmail.com>
2021-09-15 02:40:31 +0530

    script changes

--- saveImgTar.sh
+++ saveImgTar.sh
@@ -4,6 +4,4 @@ cp loadScript.sh tars/loadScript.sh
 docker save -o tars/vote-app.tar vasanth12/vote-app
 docker save -o tars/node-app.tar vasanth12/node-app
 docker save -o tars/worker-app.tar vasanth12/worker-app
-docker rmi -f vasanth12/vote-app 
-docker rmi -f vasanth12/node-app
-docker rmi -f vasanth12/worker-app 
+
