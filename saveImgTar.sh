b81bc9586c6b5ff97b128ecb42a0cd127e04f321
VasanthaRaman <vasantharaman003@gmail.com>
2021-09-15 14:20:01 +0530

    Added files

--- saveImgTar.sh
+++ saveImgTar.sh
@@ -1,7 +1,7 @@
 [ -d "tars" ] && rm -rf tars 
 mkdir tars
 cp loadScript.sh tars/loadScript.sh
-docker save -o tars/vote-app.tar vasanth12/vote-app
-docker save -o tars/node-app.tar vasanth12/node-app
-docker save -o tars/worker-app.tar vasanth12/worker-app
+docker save -o tars/vote-app.tar vasanth-vote-app
+docker save -o tars/node-app.tar vasanth-node-app
+docker save -o tars/worker-app.tar vasanth-worker-app
 
