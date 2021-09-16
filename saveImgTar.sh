f3b7c11dd2f443f9c90b47f95ae0a07c688a715f
Vasantha Raman.A <43679625+VasanthaRaman@users.noreply.github.com>
2021-09-16 17:36:57 +0530

    update sh

--- saveImgTar.sh
+++ saveImgTar.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 [ -d "tars" ] && rm -rf tars 
 mkdir tars
 cp loadScript.sh tars/loadScript.sh
 docker save -o tars/vote-app.tar vasanth-vote-app
 docker save -o tars/node-app.tar vasanth-node-app
 docker save -o tars/worker-app.tar vasanth-worker-app
-
+echo 'hello'
